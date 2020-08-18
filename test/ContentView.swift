5750a15101e7ba6be2aec2f5a0d0faac605de9df
liutuanqi <826725672@qq.com>
2020-08-19 03:25:55 +0800

    first

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Tuanqi Liu on 2020/2/8.
+//  Copyright © 2020 Tuanqi Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!哈哈哈")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
